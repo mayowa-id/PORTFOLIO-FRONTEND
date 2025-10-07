bf570ec78e7e6dc16384b083ed6335868a4753f4
Olumayowa Idowu <idowumayowa02@gmail.com>
2025-10-07 13:10:07 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TerminalShell from './components/TerminalShell'
+
+
+export default function App(){
+return (
+<div className="app-root">
+<TerminalShell />
+</div>
+)
+}
