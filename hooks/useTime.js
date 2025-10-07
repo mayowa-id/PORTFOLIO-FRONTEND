d8ecd92428a76a8991cd4bfbd6dc9586afe1bcd7
Olumayowa Idowu <idowumayowa02@gmail.com>
2025-10-07 13:05:38 +0100

    Delete hooks directory

--- hooks/useTime.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react'
-export default function useTime(){
-const [t, setT] = useState(()=> new Date().toLocaleTimeString())
-useEffect(()=>{
-const id = setInterval(()=> setT(new Date().toLocaleTimeString()), 1000)
-return ()=> clearInterval(id)
-},[])
-return t
-}
